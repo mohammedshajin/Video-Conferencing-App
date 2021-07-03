bc87b7c5484338e05f133f9592bb3f50ca268d75
mohammed shajin <mohammedshajin1997@gmail.com>
2021-07-03 10:10:18 +0530

    js added

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,26 @@
 console.log('In main.js!')
+
+
+var usernameInput = document.querySelector('#username');
+var btnJoin = document.querySelector('#btn-join');
+
+var username;
+
+btnJoin.addEventListener('click', () => {
+    username = usernameInput.value;
+
+    console.log('username: ', username);
+
+    if(username == ''){
+        return;
+    }
+    usernameInput.value = '';
+    usernameInput.disabled = true;
+    usernameInput.style.visibilty = 'hidden';
+
+    btnJoin.disabled = true;
+    btnJoin.style.visibilty = 'hidden';
+
+    var labelUsername = document.querySelector('#label-username');
+    labelUsername.innerHTML = username;
+});
